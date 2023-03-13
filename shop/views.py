e90fc2cc9d4edc373728a25afd7cc472557e348d
oficialzr <49753192+oficialzr@users.noreply.github.com>
2023-03-13 22:24:32 +0300

    Add a page for every product

--- shop/views.py
+++ shop/views.py
@@ -13,5 +13,9 @@ def home(request, category_slug=None):
 	return render(request, 'home.html', {'products': products, 'category': category_page})
 
 
-def product(request):
-	return render(request, 'product.html')
+def product(request, category_slug, product_slug):
+	try:
+		product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+	except Exception as e:
+		raise e
+	return render(request, 'product.html', {'product': product})
